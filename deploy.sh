a8dd07c539ebd71b1ff8b4764c83991d5c3c1bdb
sangmin.lee <hwon3794@gmail.com>
2022-01-20 16:32:37 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-  docker-compose -f docker-compose-green.yml up -d --build
-  sleep 10
+#!/bin/bash
+
+
+   
+    echo "Start Green"
+    docker-compose -f docker-compose-green.yml up -d --build
 done
